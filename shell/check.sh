8deaf2c14be8c111d05bc0851b954105f1d92d0d
Frederik Peter Høngaard <mail@frederikhoengaard.com>
2023-05-05 00:08:04 +0200

    Update

--- shell/check.sh
+++ shell/check.sh
@@ -5,13 +5,13 @@ export PIPENV_VERBOSITY=-1
 set -e
 
 echo "--- black ---"
-pipenv run black --line-length 88 --check python/src/main/
+pipenv run black --line-length 88 --check python/src/lazylearn/
 pipenv run black --line-length 88 --check python/src/test/
 echo "--- isort ---"
-pipenv run isort python/src/main/ --multi-line 3 --profile black --check
+pipenv run isort python/src/lazylearn/ --multi-line 3 --profile black --check
 pipenv run isort python/src/test/ --multi-line 3 --profile black --check
 echo "--- flake8 ---"
-pipenv run flake8 python/src/main/
+pipenv run flake8 python/src/lazylearn/
 pipenv run flake8 python/src/test/
 echo "--- pytest ---"
 if [[ "$OSTYPE" == "msys" ]]; then
--- shell/tidy.sh
+++ shell/tidy.sh
@@ -6,8 +6,8 @@ export PIPENV_VERBOSITY=-1
 set -e
 
 echo "--- black ---"
-pipenv run black --line-length 88 python/src/main/
+pipenv run black --line-length 88 python/src/lazylearn/
 pipenv run black --line-length 88 python/src/test/
 echo "--- isort ---"
-pipenv run isort python/src/main/ --multi-line 3 --profile black
+pipenv run isort python/src/lazylearn/ --multi-line 3 --profile black
 pipenv run isort python/src/test/ --multi-line 3 --profile black
