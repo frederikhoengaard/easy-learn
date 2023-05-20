cd2c4ffc9d981dee126cf7c1b88a869abb58693d
Frederik Peter Høngaard <mail@frederikhoengaard.com>
2023-05-20 23:23:20 +0200

    linting

--- python/src/lazylearn/regression/models/randomforest/random_forest_steps/regressor_step.py
+++ python/src/lazylearn/regression/models/randomforest/random_forest_steps/regressor_step.py
@@ -10,8 +10,8 @@ class RandomForestRegressorStep(PipelineStep):
     def fit(self, pipeline: RegressionPipeline):
         self.regressor.fit(X=pipeline.train_features_df, y=pipeline.train_targets)
 
-        #y_hat = self.regressor.predict(X=pipeline.holdout_features_df)
-        #pipeline.holdout_score = mean_absolute_error(pipeline.holdout_targets, y_hat)
+        # y_hat = self.regressor.predict(X=pipeline.holdout_features_df)
+        # pipeline.holdout_score = mean_absolute_error(pipeline.holdout_targets, y_hat)
 
     def predict(self, pipeline: RegressionPipeline):
         raise NotImplementedError
