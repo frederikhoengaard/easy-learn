5544e228c034beb1999d6e7f27509ba079e218d3
Frederik Peter Høngaard <mail@frederikhoengaard.com>
2023-05-29 23:12:29 +0200

    Update splitters.py

--- python/src/lazylearn/model_selection/splitters.py
+++ python/src/lazylearn/model_selection/splitters.py
@@ -20,6 +20,9 @@ def time_test_train_splitter(dataset: Dataset, test_size: float) -> Dataset:
 
     if isinstance(test_size, float):
         test_size = int(n * test_size)
+    elif isinstance(test_size, int):
+        assert test_size < n
+        # TODO: Implement a warning of test size seems too big
 
     dataset.partitions["test"] = dataset.df.tail(test_size)
     dataset.partitions["train"] = dataset.df.tail(n - test_size)
